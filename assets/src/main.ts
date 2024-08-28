2a73e31a90a6ef2bacbe06b50f9f5274cf889638
Abderrahmane ELOUALI <abderrahmane.elouadli@waveagency.fr>
2024-08-28 18:58:42 +0100

    Upload The Project

---
+++ assets/src/main.ts
@@ -0,0 +1,59 @@
+let allList = document.querySelectorAll("li") as NodeListOf<HTMLLIElement>;
+let result = document.querySelector(".result") as HTMLDivElement;
+// window.localStorage.clear();
+if (window.localStorage.color) {
+  result.style.backgroundColor = window.localStorage.color;
+  if (
+    window.localStorage.color === "blue" ||
+    window.localStorage.color === "black"
+  ) {
+    console.log(window.localStorage.color);
+    result.style.color = "white";
+    result.innerHTML = `${window.localStorage.color[0].toUpperCase()}${window.localStorage.color.slice(
+      1
+    )}`;
+  } else {
+    result.style.color = "black";
+    result.innerHTML = `${window.localStorage.color[0].toUpperCase()}${window.localStorage.color.slice(
+      1
+    )}`;
+  }
+  result.innerHTML = `${window.localStorage.color[0].toUpperCase()}${window.localStorage.color.slice(
+    1
+  )}`;
+  allList.forEach((li) => {
+    li.classList.remove("active");
+  });
+  allList.forEach((li) => {
+    if (window.localStorage.color === li.dataset.color) {
+      li.className = "active";
+    }
+  });
+}
+
+allList.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    allList.forEach((li) => {
+      li.classList.remove("active");
+    });
+    const target = e.target as HTMLLIElement;
+    target.classList.add("active");
+    const color = target.dataset.color;
+    if (color) {
+      window.localStorage.setItem("color", color);
+      result.style.backgroundColor = window.localStorage.color;
+      if (color === "blue" || color === "black") {
+        console.log(window.localStorage.color);
+        result.style.color = "white";
+        result.innerHTML = `${window.localStorage.color[0].toUpperCase()}${window.localStorage.color.slice(
+          1
+        )}`;
+      } else {
+        result.style.color = "black";
+        result.innerHTML = `${window.localStorage.color[0].toUpperCase()}${window.localStorage.color.slice(
+          1
+        )}`;
+      }
+    }
+  });
+});
